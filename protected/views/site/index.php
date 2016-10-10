5eee162b577dfdb42c75899f48d81db45cdec9c7
aydin <aydin.abdnia@gmail.com>
2016-10-10 17:37:29 +0330

    add selected 2 extension yii

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -7,7 +7,14 @@ $this->pageTitle=Yii::app()->name;
 
 
 
-
+$tags=array('Satu','Dua','Tiga');
+echo CHtml::textField('test','',array('id'=>'test'));
+$this->widget('ext.select2.ESelect2',array(
+    'selector'=>'#test',
+    'options'=>array(
+        'tags'=>$tags,
+    ),
+));
 ?>
 
 
